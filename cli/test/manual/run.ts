d0ebc8f6b3931f2658fce29c87a20f3cd1822a8f
deaMorta <freelance.starterpack08@gmail.com>
2022-04-15 00:11:25 +0800

    fix: valtio downgrade to 1.3.1

--- cli/test/manual/run.ts
+++ cli/test/manual/run.ts
@@ -1,30 +1,13 @@
 import chalk from "chalk";
-import fs from "fs-extra";
 import Cli from "../../src/cli.js";
-import { createApp, createTestDir } from "../utils/generator.js";
+import { createTestDir } from "../utils/generator.js";
 
 // Add more options here
 const main = async () => {
   const { dir } = await createTestDir("manual-test", {
     removeDir: true,
-    pkg: {
-      vx: {
-        templatesPaths: ["./test-templates"],
-      },
-    },
+    removePkg: true,
   });
-
-  await createApp("test-template-app", ".test/manual-test/test-templates", {});
-  await fs.writeFile(
-    ".test/manual-test/test-templates/test-template-app/test-file.txt",
-    "test",
-    "utf8"
-  );
-
-  await fs.writeFile(
-    ".test/manual-test/test-templates/test-template-app/.vxignore",
-    "**/test-file.txt"
-  );
 
   const cli = new Cli(dir);
 
